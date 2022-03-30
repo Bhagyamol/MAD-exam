cb587ad579e79764b4a9108e34376f99615fddc1
Bhagyamol <74665675+Bhagyamol@users.noreply.github.com>
2022-03-30 12:31:59 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.sjcet.regform;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    public void click(View view) {
+        }
+
+
+    public void cancel(View view) {
+
+    }
+}
+
